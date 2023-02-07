751e844fb21d33daf187474cf67afab2e25f8779
fiantonumber1 <87689030+fiantonumber1@users.noreply.github.com>
2023-02-07 07:22:41 +0700

    Update
    tambahan dan perbaikan

--- Project_optimasi/GA_GTB_Full/savemodel.py
+++ Project_optimasi/GA_GTB_Full/savemodel.py
@@ -6,7 +6,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow import keras
 
-wb = openpyxl.load_workbook('A.xlsx')
+wb = openpyxl.load_workbook(r'/content/A.xlsx')
 sheet = wb.active
 data = []
 for row in sheet.iter_rows(values_only=True):
@@ -21,8 +21,6 @@ uv = a[7000:8000,:8]
 yv = a[7000:8000,8:11]
 us = a[3:8000,:8]
 ys = a[3:8000,8:11]
-u = np.array(x, dtype=np.float32)   #8x1
-u = u.reshape(1, -1) # reshape u to have a shape of (1, 8)
 us=np.array(us, dtype=np.float32)   #8x6997
 uv=np.array(uv, dtype=np.float32)   #8x1000
 ys=np.array(ys, dtype=np.float32)   #3x6997
